cffaf968e6dda1dc06149747cf0b8ee139672e8d
karthikeyansa8 <karthikeyansa8@gmail.com>
2026-04-09 21:39:09 +0530

    mod allowd hosts

--- rynixsoft/settings.py
+++ rynixsoft/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["https://digital-task-management-system.onrender.com" , "digital-task-management-system.onrender.com"]
 
 # Application definition
 
